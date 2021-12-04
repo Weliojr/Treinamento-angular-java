06010dae9ce4c9d7cb6d959c548ad22b928f6d53
Francisco Welio <welio.juniorw@gmail.com>
2021-12-04 10:19:08 -0300

    nova atualização 04/12/21

--- meubancoangular/src/app/app.module.ts
+++ meubancoangular/src/app/app.module.ts
@@ -5,12 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ExtratoComponent } from './pages/extrato/extrato.component';
+import { DepositoComponent } from './pages/deposito/deposito.component';
+import { SaqueComponent } from './pages/saque/saque.component';
+
+const routes: Routes = [
+  { path: ' ', component: HomeComponent, pathMatch: 'full' },
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'saque', component: SaqueComponent },
+  { path: 'deposito', component: DepositoComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ExtratoComponent,
+    DepositoComponent,
+    SaqueComponent
   ],
   imports: [
     BrowserModule,
